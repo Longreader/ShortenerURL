35e9fbe697c8612b8c83585eec785bc2d4dd03db
Kirichek Alexey <rokirokz@mail.ru>
2023-09-05 17:59:59 +0300

    Добавлены тесты

--- config/config_test.go
+++ config/config_test.go
@@ -3,6 +3,8 @@ package config_test
 import (
 	"flag"
 	"testing"
+
+	"github.com/Longreader/go-shortener-url.git/config"
 )
 
 func TestFlags(t *testing.T) {
@@ -11,4 +13,48 @@ func TestFlags(t *testing.T) {
 	if *val != ":4040" {
 		t.Errorf("Expected port :4040, got %s", *val)
 	}
+}
+
+func TestGetValue(t *testing.T) {
+	type want struct {
+		value string
+	}
+
+	tests := []struct {
+		name string
+		foo  func() string
+		want want
+	}{
+		{
+			name: "positive test #1 GetAddress",
+			foo:  config.GetAddress,
+			want: want{
+				value: "127.0.0.1:8080",
+			},
+		},
+		{
+			name: "positive test #2 GetURL",
+			foo:  config.GetURL,
+			want: want{
+				value: "http://127.0.0.1:8080/",
+			},
+		},
+		{
+			name: "positive test #2 GetPath",
+			foo:  config.GetStoragePath,
+			want: want{
+				value: "",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := tt.foo()
+			if result != tt.want.value {
+				t.Errorf("Expected value code %s, got %s", tt.want.value, result)
+			}
+		})
+	}
+
 }
