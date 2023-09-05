44e4785214c8b512a74acc236f0d2e28f547d45c
Kirichek Alexey <rokirokz@mail.ru>
2023-09-05 17:30:47 +0300

    Добавлен тест

--- internal/storage/storage_test.go
+++ internal/storage/storage_test.go
@@ -1,18 +1,21 @@
 package storage_test
 
 import (
+	"os"
 	"testing"
+
+	"github.com/Longreader/go-shortener-url.git/internal/storage"
 )
 
 func TestStorage(t *testing.T) {
-	// var valueURL = "value"
-	// var keyURL = "key"
-	// os.Setenv("SERVER_ADDRESS", "127.0.0.1:8080")
-	// os.Setenv("BASE_URL", "http://127.0.0.1:8080/")
-	// os.Setenv("FILE_STORAGE_PATH", "log.log")
-	// store := storage.New()
-	// store.Set(keyURL, valueURL)
-	// if storeVal, _ := store.Get(keyURL); storeVal != valueURL {
-	// 	t.Errorf("Expected value %s, got %s", storeVal, valueURL)
-	// }
+	var valueURL = "value"
+	var keyURL = "key"
+	os.Setenv("SERVER_ADDRESS", "127.0.0.1:8080")
+	os.Setenv("BASE_URL", "http://127.0.0.1:8080/")
+	os.Setenv("FILE_STORAGE_PATH", "log.log")
+	store := storage.New()
+	store.Set(keyURL, valueURL)
+	if storeVal, _ := store.Get(keyURL); storeVal != valueURL {
+		t.Errorf("Expected value %s, got %s", storeVal, valueURL)
+	}
 }
